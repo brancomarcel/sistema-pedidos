f3c451dd9c1b2f1f78cc3f55ced1153aa3b1479f
marcelsbranco <marcelsbranco@gmail.com>
2024-09-24 16:15:57 -0300

    estrutura inicial

---
+++ src/test/java/com/marcelsistemapedidos/SistemaPedidosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcelsistemapedidos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaPedidosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
